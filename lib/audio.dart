1ea2dc95b31e5e208da207d7627cd81b4cd2d766
myron6520 <100897740+myron6520@users.noreply.github.com>
2026-01-28 17:50:41 +0800

    Update audio.dart

--- lib/audio.dart
+++ lib/audio.dart
@@ -5,6 +5,12 @@ class Audio {
   late String path;
   late int duration;
   late double speed;
+  late String text;
+  late String textZh;
+  late String textEn;
+  late String textVi;
+  late String textZhCn;
+  late String textZhTw;
   Audio.fromMap(Map info) {
     uuid = "${info['uuid'] ?? ''}";
     path = "${info['path'] ?? ''}";
@@ -12,6 +18,12 @@ class Audio {
     assetPackage = "${info['assetPackage'] ?? ''}";
     duration = int.tryParse("${info['duration'] ?? ''}") ?? 0;
     speed = double.tryParse("${info['speed'] ?? ''}") ?? 1;
+    text = "${info['text'] ?? ''}";
+    textZh = "${info['text_zh'] ?? ''}";
+    textEn = "${info['text_en'] ?? ''}";
+    textVi = "${info['text_vi'] ?? ''}";
+    textZhCn = "${info['text_zh-CN'] ?? ''}";
+    textZhTw = "${info['text_zh-TW'] ?? ''}";
   }
   Map toMap() => {
         'uuid': uuid,
@@ -20,5 +32,14 @@ class Audio {
         'path': path,
         'duration': duration,
         'speed': speed,
+        'text': text,
+        'text_zh': textZh,
+        'text_en': textEn,
+        'text_vi': textVi,
+        'text_zh-CN': textZhCn,
+        'text_zh-TW': textZhTw,
       };
+  bool get isAsset => asset.isNotEmpty;
+  bool get isLocal => path.isNotEmpty;
+  bool get useTts => !isAsset && !isLocal;
 }
